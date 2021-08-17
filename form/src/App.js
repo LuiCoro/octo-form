d63f4a886b203199ce083dd560050efc0ced1d12
LuiCoro <luiacorona029@icloud.com>
2021-08-16 20:09:22 -0700

    'Linking' * Linked SimpleForm.js to App.js via Route * SimpleForm.js created with placeholder

--- form/src/App.js
+++ form/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { Route, Link} from 'react-router-dom'
+
+// Forms Examples To Look At
+import SimpleForm from "./Components/SimpleForm/SimpleForm";
+
 
 const App = () => {
     return (
         <div>
             <h1>
-                Hello World!
+                Form Viewer
             </h1>
+            <p>
+                Click on a button to view its example!
+            </p>
+            <Link to="/simple-form">
+                <button>Simple Form</button>
+            </Link>
+
+            <Route exact path='/'>
+                Home Page
+            </Route>
+            <Route path='/simple-form'>
+                <SimpleForm/>
+            </Route>
         </div>
+
     )
 }
 
---
+++ form/src/Components/SimpleForm/SimpleForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <h2>
+                I am simple form
+            </h2>
+        </div>
+    )
+}
+
+export default SimpleForm
