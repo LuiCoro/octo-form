70815c7d0554e782528101ffe1ecec2397ebeaf4
LuiCoro <luiacorona029@icloud.com>
2021-08-16 23:15:33 -0700

    'Bare-Bones' * Added Skeleton inside InputComponent.js
    'Inputs Added'
    * Text
    * Email
    * Select
    * Checkbox
    * Radio

--- form/src/Components/InputsComponents/InputComponent.js
+++ form/src/Components/InputsComponents/InputComponent.js
@@ -3,7 +3,84 @@ import React from 'react'
 const Inputs = () => {
     return (
         <div>
-            <h1>A lot of divs</h1>
+            <h1>Input Examples!</h1>
+            <div>
+                <form>
+                    <h2>Text Input</h2>
+                    <label>
+                        Name: <input type="text" name="name" placeholder="Enter Your Name"/>
+                    </label>
+
+                    <h2>Email Input</h2>
+                    <label>
+                        Email: <input type="email" name="email" placeholder="Enter Your Email"/>
+                    </label>
+
+                    <h2>Select</h2>
+                    <label>
+                        Select Your Skill:
+                        <select name="skill">
+                            <option value="select">Select</option>
+                            <option value='rage'>Rage</option>
+                            <option value="lazy">Lazy</option>
+                            <option value="smart">Smart</option>
+                            <option value="creative">Creative</option>
+                        </select>
+                    </label>
+
+                    <h2>Check Box</h2>
+                    <label>
+                        Xbox <input type='checkbox' name='xbox'/>
+                    </label>
+
+                    <label>
+                        PlayStation <input type='checkbox' name='sony'/>
+                    </label>
+
+                    <label>
+                        PC <input type='checkbox' name='pc'/>
+                    </label>
+
+                    <label>
+                        Nintendo <input type='checkbox' name='nintendo'/>
+                    </label>
+
+                    <h2>PassWord</h2>
+                    <label>
+                        Password: <input type="password" name='password' placeholder='Enter Password'/>
+                    </label>
+
+                    <h2>Radio Inputs</h2>
+                    <div>
+                        <label>
+                            Pizza <input type='radio' name='favfood'/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Hotdogs <input type='radio' name='favfood'/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Hamburger <input type='radio' name='favfood'/>
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Tacos <input type='radio' name='favfood'/>
+                        </label>
+                    </div>
+
+                    <br/>
+
+                    <button id='inputsBTN'>Submit</button>
+
+                </form>
+            </div>
         </div>
     )
 }
